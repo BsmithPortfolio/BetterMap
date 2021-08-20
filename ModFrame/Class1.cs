25a36d7d765a86086c8ed794eb7abe65827226df
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2021-08-20 14:58:30 -0500

    Rotational function

--- ModFrame/Class1.cs
+++ ModFrame/Class1.cs
@@ -25,6 +25,8 @@ namespace BetterMaps
             Harmony harmony = new(ModGUID);
             harmony.PatchAll(assembly);
             LoadAssets();
+            On.Minimap.UpdatePlayerMarker += Minimap_UpdatePlayerMarker;
+            On.Minimap.CenterMap += Minimap_CenterMap;
         }
         public void LoadAssets()
         {
@@ -72,7 +74,39 @@ namespace BetterMaps
                 __instance.m_windMarker = BetterMapper.internalWindDir;
             }
         }
+        private void Minimap_UpdatePlayerMarker(On.Minimap.orig_UpdatePlayerMarker orig, Minimap self, Player player, Quaternion playerRot)
+        {
+            if (self.m_mode == Minimap.MapMode.Small)
+            {
+                self.m_smallMarker.rotation = Quaternion.Euler(0, 0, 0);
+                Ship controlledShip = player.GetControlledShip();
+                if ((bool)controlledShip)
+                {
+                    self.m_smallShipMarker.gameObject.SetActive(value: true);
+                    float yawShip = controlledShip.GetShipYawAngle();
+                    self.m_smallShipMarker.transform.rotation = Quaternion.Euler(0, 0, yawShip);
+                }
+                else
+                {
+                    self.m_smallShipMarker.gameObject.SetActive(value: false);
+                }
+            } else
+            {
+                orig(self, player, playerRot);
+            }
+        }
         
+        
+        private void Minimap_CenterMap(On.Minimap.orig_CenterMap orig, Minimap self, Vector3 centerPoint)
+        {
+            self.m_mapImageSmall.transform.rotation = Quaternion.Euler(0, 0, Player.m_localPlayer.m_eye.transform.rotation.eulerAngles.y);
+            self.m_pinRootSmall.transform.rotation = Quaternion.Euler(0, 0, Player.m_localPlayer.m_eye.transform.rotation.eulerAngles.y);
+            for (int i = 0; i < self.m_pinRootSmall.childCount; i++)
+            {
+                self.m_pinRootSmall.transform.GetChild(i).transform.rotation = Quaternion.identity;
+            }
+            orig(self, centerPoint);
+        }
         
     }
 }
