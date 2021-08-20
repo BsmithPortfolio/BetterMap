b4f04bbb7bb383fea1d2efe934f4a3051de9b42e
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2021-08-20 15:59:58 -0500

    Add DragNDrop.cs monoscript for moving the map

---
+++ New Unity Project/Assets/Prefabs/MiniMap/MonoScripts/DragNDrop.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+public class DragNDrop : MonoBehaviour
+{
+ public Transform target;
+    public bool shouldReturn;
+    private bool isMouseDown;
+    private Vector3 startMousePosition;
+    private Vector3 startPosition;
+
+    internal static bool donedrag;
+    private void Update()
+    {
+        if (isMouseDown)
+        {
+            var currentPosition = Input.mousePosition;
+
+            var diff = currentPosition - startMousePosition;
+
+            var pos = startPosition + diff;
+
+            target.position = pos;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData dt)
+    {
+        isMouseDown = true;
+        donedrag = isMouseDown;
+        Debug.Log("Draggable Mouse Down");
+
+        startPosition = target.position;
+        startMousePosition = Input.mousePosition;
+    }
+
+    public void OnPointerUp(PointerEventData dt)
+    {
+        Debug.Log("Draggable mouse up");
+
+        isMouseDown = false;
+
+        donedrag = isMouseDown;
+        if (shouldReturn) target.position = startPosition;
+    }
+}
